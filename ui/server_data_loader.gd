940f7a2c81ef41fe8c1b2ef599cfd958332f4c40
Travh98 <travishunter33@gmail.com>
2024-07-29 12:02:37 -0400

    Update server_data_loader.gd

--- ui/server_data_loader.gd
+++ ui/server_data_loader.gd
@@ -7,10 +7,11 @@ signal maps_loaded(Array)
 var map_str_array: Array
 
 func _ready():
+	map_str_array.append("sa_w_level") # PVE test
+	map_str_array.append("haunted_coast")
 	map_str_array.append("good_and_nice_castle")
 	map_str_array.append("flake")
 	map_str_array.append("bonnet_shores")
-	map_str_array.append("sa_w_level") # PVE test
 	
 	#await get_tree().create_timer(1).timeout
 	send_loaded_map_names.call_deferred()
